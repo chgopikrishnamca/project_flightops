f6524a015a7e9a9b7a19e8d647a44e73cc25be8e
Gopikrishna Cherukuri <chgopikrishnamca@gmail.com>
2022-06-20 21:13:46 +0530

    Changes in models.py

--- flightservices/models.py
+++ flightservices/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 from rest_framework.response import Response
 
-def calculateTime(flightDepartureTime, flightArrivalTime):
-    """ To Calculate the number of hours and days between the given dates and times. For now, giving manual value."""
-    flightTravelDuration = 2
-    return flightTravelDuration
 
 def ValidateFields(passengerPhone):
     if len(passengerPhone)!=10:
@@ -13,20 +9,20 @@ def ValidateFields(passengerPhone):
 
 class FlightDetailsModel(models.Model):
     """ Flight Details Model"""
-    flightNum = models.IntegerField(max_length=10)
+    flightNum = models.IntegerField()
     flightService = models.CharField(max_length=30)
     flightArrivalTime = models.DateTimeField()
     flightDepartureTime = models.DateTimeField()
     flightSource = models.CharField(max_length=20)
     flightDestination = models.CharField(max_length=20)
-    flightTravelDuration = calculateTime(flightDepartureTime, flightArrivalTime)
+    flightTravelDuration = models.IntegerField(default=2)
 
 class PassengerDetailsModel(models.Model):
     """ Passenger Details Model"""
     passengerName = models.CharField(max_length=30)
-    passengerEmail = models.EmailField(Null=False)
-    passengerPhone = models.IntegerField(max_length=10)
+    passengerEmail = models.EmailField()
+    passengerPhone = models.IntegerField()
 
 class ReservationModel(models.Model):
-    flight = models.oneToOneField(FlightDetailsModel, on_delete=models.CASCADE)
+    flight = models.OneToOneField(FlightDetailsModel, on_delete=models.CASCADE)
     passenger = models.OneToOneField(PassengerDetailsModel, on_delete=models.CASCADE)
