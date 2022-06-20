e2cf4ec953cf84d6fe9f635d04200a54e87a4138
Gopikrishna Cherukuri <chgopikrishnamca@gmail.com>
2022-06-20 18:46:03 +0530

    Models added

--- flightservices/models.py
+++ flightservices/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from rest_framework.response import Response
 
-# Create your models here.
+def calculateTime(flightDepartureTime, flightArrivalTime):
+    """ To Calculate the number of hours and days between the given dates and times. For now, giving manual value."""
+    flightTravelDuration = 2
+    return flightTravelDuration
+
+def ValidateFields(passengerPhone):
+    if len(passengerPhone)!=10:
+        return Response("Conatct Number Invalid")
+
+
+class FlightDetailsModel(models.Model):
+    """ Flight Details Model"""
+    flightNum = models.IntegerField(max_length=10)
+    flightService = models.CharField(max_length=30)
+    flightArrivalTime = models.DateTimeField()
+    flightDepartureTime = models.DateTimeField()
+    flightSource = models.CharField(max_length=20)
+    flightDestination = models.CharField(max_length=20)
+    flightTravelDuration = calculateTime(flightDepartureTime, flightArrivalTime)
+
+class PassengerDetailsModel(models.Model):
+    """ Passenger Details Model"""
+    passengerName = models.CharField(max_length=30)
+    passengerEmail = models.EmailField(Null=False)
+    passengerPhone = models.IntegerField(max_length=10)
+
+class ReservationModel(models.Model):
+    flight = models.oneToOneField(FlightDetailsModel, on_delete=models.CASCADE)
+    passenger = models.OneToOneField(PassengerDetailsModel, on_delete=models.CASCADE)
+
+
+
